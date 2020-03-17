9b56ba4ae25e04b2b74e2cc4a9b2e2528143f1c5
aoki-yz <62273614+aoki-yz@users.noreply.github.com>
2020-03-17 17:51:07 +1030

    Add files via upload

---
+++ scripts/run_v2.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+data_dir="./imagenet"
+output_dir="./output/moco_v2"
+python -m torch.distributed.launch --master_port 12347 --nproc_per_node=8 \
+    train_v2.py \
+    --data-dir ${data_dir} \
+    --amp-opt-level O2 \
+    --output-dir ${output_dir}
+
+python -m torch.distributed.launch --master_port 12348 --nproc_per_node=4 \
+    eval_v2.py \
+    --data-dir ${data_dir} \
+    --pretrained-model ${output_dir}/current.pth \
+    --output-dir ${output_dir}/eval
+
