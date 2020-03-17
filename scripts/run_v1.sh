7ccabe19d1738ddffe88c6ccb864caf00b5a542e
aoki-yz <62273614+aoki-yz@users.noreply.github.com>
2020-03-17 17:49:30 +1030

    Update and rename train_eval_imagenet100_baseLR0.4_alpha0.99_crop0.08_k1281166_t0.1_AMPO1.sh to run_v1.sh

--- scripts/run_v1.sh
+++ scripts/run_v1.sh
@@ -1,23 +1,18 @@
 #!/bin/bash
 
-data_dir="./data/imagenet100"
-output_dir="./output/imagenet100/baseLR0.4_alpha0.99_crop0.08_kall_t0.1_AMPO1"
+data_dir="./imagenet"
+output_dir="./output/moco_v1"
 python -m torch.distributed.launch --master_port 12347 --nproc_per_node=8 \
-    train.py \
+    train_v1.py \
     --data-dir ${data_dir} \
-    --dataset imagenet100 \
-    --base-learning-rate 0.4 \
-    --alpha 0.99 \
-    --crop 0.08 \
-    --nce-k 126689 \
-    --nce-t 0.1 \
-    --amp-opt-level O1 \
+    --amp-opt-level O2 \
     --output-dir ${output_dir}
 
 python -m torch.distributed.launch --master_port 12348 --nproc_per_node=4 \
     eval.py \
-    --dataset imagenet100 \
     --data-dir ${data_dir} \
     --pretrained-model ${output_dir}/current.pth \
     --output-dir ${output_dir}/eval
+
+
 
